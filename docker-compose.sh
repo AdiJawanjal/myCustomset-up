e13d3fa35a870103d1016405af9a0261f5333d5a
Aditya Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2025-06-30 01:56:09 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,7 @@
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 mkdir -p $DOCKER_CONFIG/cli-plugins
 
+
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
   -o $DOCKER_CONFIG/cli-plugins/docker-compose
 
